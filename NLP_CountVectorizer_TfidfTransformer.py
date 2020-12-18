fcf05e604091dd983f56197f288b8830c8298bf9
Ella <57242849+HaoxueChang@users.noreply.github.com>
2020-12-18 14:24:03 +0800

    Update NLP_CountVectorizer_TfidfTransformer.py

--- NLP_CountVectorizer_TfidfTransformer.py
+++ NLP_CountVectorizer_TfidfTransformer.py
@@ -1,6 +1,21 @@
 #CountVectorizer 用于将一个或多个文本转换为词频矩阵
 #将文本分词后，将所有文本中出现的所有词作为一个字典，每个文本的词向量的长度是该词典的长度，值是词频，即该词在该文本出现的次数。
 #拿出每个文本词向量中不为0的部分就是该文本的词频统计
+#参数设置：
+#class sklearn.feature_extraction.text.CountVectorizer(input='content', encoding='utf-8', decode_error='strict', 
+#                        strip_accents=None, lowercase=True, preprocessor=None, tokenizer=None, stop_words=None, 
+#                        token_pattern='(?u)\b\w\w+\b', ngram_range=(1, 1), analyzer='word', max_df=1.0, min_df=1, 
+#                        max_features=None, vocabulary=None, binary=False, dtype=<class 'numpy.int64'>)
+# input, encoding，strip_accents使用默认设置
+# decode_error: strict,遇到不能解码的会报错；ingore会忽略解码错误
+# analyzer，可设为word/char/char_wb
+# ngram_range:词组切分长度范围
+# lowercase：将所有字符变成小写
+# token_pattern：表示token的正则表达式，analyzer要设置为word,默认'(?u)\b\w\w+\b'，表示选择2个或两个以上的字母或数字作为token,标点符号默认为token分隔符，而不会作为token
+# max_df: [0.0 1.0]的float，默认为1.0。这个参数的作用是作为一个阈值，当构造语料库的关键词集的时候，如果某个词的document frequence大于max_df，这个词不会被当作关键词。
+# min_df：类似于max_df，不同之处在于如果某个词的document frequence小于min_df，则这个词不会被当作关键词
+# max_features：默认为None，可设为int，对所有关键词的term frequency进行降序排序，只取前max_features个作为关键词集
+# dtype：使用CountVectorizer类的fit_transform()或transform()将得到一个文档词频矩阵，dtype可以设置这个矩阵的数值类型
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer = CountVectorizer(min_df=1)
 corpus = [ 'This is the first document.',
