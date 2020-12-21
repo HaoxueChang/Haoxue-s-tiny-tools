e55ec17ff7874edd933c2aadabdd49c95d880072
Ella <57242849+HaoxueChang@users.noreply.github.com>
2020-12-21 10:34:19 +0800

    Update how_to_regular_expression.py

--- how_to_regular_expression.py
+++ how_to_regular_expression.py
@@ -19,3 +19,20 @@ re.sub(r"'[Ss]", 'S', text)
 
 #匹配字母数字与非字母数字
 # https://blog.csdn.net/uvyoaa/article/details/80854459
+# \b,\B是单词边界，不匹配任何字符
+# \b表示字母数字与非字母数字的边界，非字母数字与字母数字的边界
+# \B表示字母数字与字母数字的边界，非字母数字与非字母数字的边界
+re.split('123\\b','==123!! abc123. 123. 123abc. 123')
+re.split(r'123\b','==123!! abc123. 123. 123abc. 123')
+# output:['==', '!! abc', '. ', '. 123abc. ', '']
+re.split('\\b123\\b','123 ==123!! abc123.123.123abc.123')
+# output:['', ' ==', '!! abc123.', '.123abc.', '']
+re.split('\\b123=\\b','==123!! abc123,123,123=abc,123')
+# output:['==123!! abc123,123,', 'abc,123']
+re.split(r'pyc\B','1pycthon py5 2pyc342 pyc1py2py4 pyp3 3pyc# pyc')
+# output:['1', 'thon py5 2', '342 ', '1py2py4 pyp3 3pyc# pyc']
+#对比以下两个表达式：
+re.split('\\b123=\\b','==123!! abc123,123,123==abc,123')  #output:['==123!! abc123,123,123==abc,123']
+re.split('\\b123=\\B','==123!! abc123,123,123==abc,123')  #output:['==123!! abc123,123,', '=abc,123']
+#下划线和字母数字算作一类
+re.split('123_\\b','==123!! abc 123_123_. 123_. 123abc. 123') #output:['==123!! abc 123_', '. ', '. 123abc. 123']
