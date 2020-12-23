92cdd2277a28299c17285876b1287e7903dbcc92
Ella <57242849+HaoxueChang@users.noreply.github.com>
2020-12-23 10:43:59 +0800

    Update how_to_regular_expression.py

--- how_to_regular_expression.py
+++ how_to_regular_expression.py
@@ -36,3 +36,12 @@ re.split('\\b123=\\b','==123!! abc123,123,123==abc,123')  #output:['==123!! abc1
 re.split('\\b123=\\B','==123!! abc123,123,123==abc,123')  #output:['==123!! abc123,123,', '=abc,123']
 #下划线和字母数字算作一类
 re.split('123_\\b','==123!! abc 123_123_. 123_. 123abc. 123') #output:['==123!! abc 123_', '. ', '. 123abc. 123']
+
+# （?u),该表达式打开re.U（re.UNICODE）标志,根据Unicode解释字符集
+# https://docs.python.org/2/library/re.html#re.U
+#re.I, ignore case, perform case-insenstive matching
+#re.L,locale,做本地化识别
+#re.M,mulitline,多行匹配，影响'^'和'$'
+#re.S,使'.'可以匹配包括换行符在内的所有字符，不设置这个flag,'.'匹配除换行符之外的所有字符
+#re.X,该标志可以使用更灵活的方式使表达式更易于理解。这一标志使得我们可以将表达式分开并添加注释。除了字符类中的空格，添加了转义符的空格，或者在token *?,(?:,(?P<...>中，别的空格会被忽略，
+
